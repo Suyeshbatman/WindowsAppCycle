48552e6800a2899374e0c38270a9379a15111907
Suyeshbatman <suyesh.bhatta199@gmail.com>
2018-11-12 18:02:33 +0545

    Add files via upload
    First Data Read form

---
+++ WindowsAppCycle/Wform/Wform/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Wform
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtRich = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtRich
+			// 
+			this.txtRich.Location = new System.Drawing.Point(78, 71);
+			this.txtRich.Name = "txtRich";
+			this.txtRich.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+			this.txtRich.Size = new System.Drawing.Size(648, 316);
+			this.txtRich.TabIndex = 0;
+			this.txtRich.Text = "";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(78, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(98, 44);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Read";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.txtRich);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox txtRich;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ WindowsAppCycle/Wform/Wform/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Wform
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string filePath = @"C:\Users\My computer\Desktop\Data for Assignment\ASEB Data\Exercises2009_Duncan Mullier\09072501.hrm"; // agadi @ lagayo vane pachi single \ halle pugcha. if @ halena vane double \\ halna parcha
+
+
+			if (File.Exists(filePath))
+			{
+				processFile(filePath);
+			}
+			else
+			{
+				txtRich.Text = "File not found.";
+			}
+		}
+
+		public void processFile(string filePath)
+		{
+			// string oneline;
+			//int lineNo = 1;
+			try
+			{
+				/* // Reading all the text using ReadAllText Method
+                string fileContent = File.ReadAllText(filePath);
+                txtDisplay.Text = fileContent;
+                */
+
+
+				//Reading all the text and display in textbox by using ReadAllLines
+				string[] filelines = File.ReadAllLines(filePath);
+				string data = "";
+				int x = 0;
+				int y = 0;
+				foreach (string line in filelines)
+				{
+					if (line.Equals("[HRData]"))
+					{
+						x = y;
+					}
+					if (x > 0)
+					{
+						data += line + "\r\n";
+					}
+
+					y++;
+				}
+				txtRich.Text = x + data;
+
+				/*
+                StreamReader sr = new StreamReader(filePath);
+                string data = "";
+                while ((oneline = sr.ReadLine()) != null)
+                {
+                    string[] words = oneline.Split(' ');
+                    for(int x = 0; x < words.Length; x++)
+                    {
+                        data += words[x] + "\r\n";
+                    }
+                }
+                txtDisplay.Text = data;
+                */
+			}
+			catch
+			{
+				txtRich.Text = "Exception Raised";
+			}
+		}
+
+		
+	}
+}
