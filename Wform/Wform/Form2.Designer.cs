0572e101ee480a5b47e32a23fe7a71c84e242dd9
Suyeshbatman <suyesh.bhatta199@gmail.com>
2018-11-14 00:05:46 +0545

    Add files via upload

--- Wform/Wform/Form2.Designer.cs
+++ Wform/Wform/Form2.Designer.cs
@@ -29,38 +29,84 @@
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.dataGV = new System.Windows.Forms.DataGridView();
+			this.button2 = new System.Windows.Forms.Button();
+			this.richTB = new System.Windows.Forms.RichTextBox();
+			this.richTB2 = new System.Windows.Forms.RichTextBox();
+			this.selectedColumnButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGV)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(87, 13);
+			this.button1.Location = new System.Drawing.Point(12, 210);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(161, 23);
 			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
+			this.button1.Text = "Read File";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
-			// dataGridView1
+			// dataGV
 			// 
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(31, 91);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(500, 227);
-			this.dataGridView1.TabIndex = 1;
+			this.dataGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGV.Location = new System.Drawing.Point(12, 249);
+			this.dataGV.Name = "dataGV";
+			this.dataGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.ColumnHeaderSelect;
+			this.dataGV.Size = new System.Drawing.Size(500, 189);
+			this.dataGV.TabIndex = 1;
+			
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(179, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Select";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// richTB
+			// 
+			this.richTB.Location = new System.Drawing.Point(12, 50);
+			this.richTB.Name = "richTB";
+			this.richTB.Size = new System.Drawing.Size(500, 134);
+			this.richTB.TabIndex = 3;
+			this.richTB.Text = "";
+			// 
+			// richTB2
+			// 
+			this.richTB2.Location = new System.Drawing.Point(543, 249);
+			this.richTB2.Name = "richTB2";
+			this.richTB2.Size = new System.Drawing.Size(329, 189);
+			this.richTB2.TabIndex = 4;
+			this.richTB2.Text = "";
+			// 
+			// selectedColumnButton
+			// 
+			this.selectedColumnButton.Location = new System.Drawing.Point(630, 209);
+			this.selectedColumnButton.Name = "selectedColumnButton";
+			this.selectedColumnButton.Size = new System.Drawing.Size(124, 23);
+			this.selectedColumnButton.TabIndex = 5;
+			this.selectedColumnButton.Text = "Load";
+			this.selectedColumnButton.UseVisualStyleBackColor = true;
+			this.selectedColumnButton.Click += new System.EventHandler(this.selectedColumnButton_Click);
 			// 
 			// Form2
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(561, 450);
-			this.Controls.Add(this.dataGridView1);
+			this.ClientSize = new System.Drawing.Size(896, 450);
+			this.Controls.Add(this.selectedColumnButton);
+			this.Controls.Add(this.richTB2);
+			this.Controls.Add(this.richTB);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.dataGV);
 			this.Controls.Add(this.button1);
 			this.Name = "Form2";
 			this.Text = "Form2";
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGV)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -68,6 +114,10 @@
 		#endregion
 
 		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridView dataGV;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.RichTextBox richTB;
+		private System.Windows.Forms.RichTextBox richTB2;
+		private System.Windows.Forms.Button selectedColumnButton;
 	}
 }
--- Wform/Wform/Form2.cs
+++ Wform/Wform/Form2.cs
@@ -13,6 +13,7 @@ namespace Wform
 {
 	public partial class Form2 : Form
 	{
+		public string filepath { get; set; }
 		public Form2()
 		{
 			InitializeComponent();
@@ -20,57 +21,151 @@ namespace Wform
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-		
-
-			Stream myStream = null;
-			OpenFileDialog openFileDialog1 = new OpenFileDialog();
-
-			openFileDialog1.InitialDirectory = @"C:\Users\My computer\Desktop\Data for Assignment\ASEB Data\Exercises2009_Duncan Mullier\"; // your directory is also not defined properly
-			openFileDialog1.Filter = "hrm files (*.hrm)|*.hrm|All files (*.*)|*.*";// have a look to filter as well
-			openFileDialog1.FilterIndex = 2;
-			openFileDialog1.RestoreDirectory = true;
-			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			string filePath = filepath;
+			if (File.Exists(filePath))
 			{
-				try
+				processFile(filePath);
+			}
+			else
+			{
+				richTB.Text = "File not found.";
+			}
+			button1.Visible = false;
+		}
+
+
+		public void processFile(string filePath)
+		{
+			// string oneline;
+			//int lineNo = 1;
+			try
+			{
+				/* // Reading all the text using ReadAllText Method
+                string fileContent = File.ReadAllText(filePath);
+                txtDisplay.Text = fileContent;
+                */
+
+
+				//Reading all the text and display in textbox by using ReadAllLines
+				string[] filelines = File.ReadAllLines(filePath);
+				string data = "";
+				int rows = 0;
+				int totalRows = 0;
+				int y = 0; int z = 0;
+
+				dataGV.ColumnCount = 6;
+				dataGV.Columns[0].Name = "Heart Rates";
+				dataGV.Columns[1].Name = "Speed";
+				dataGV.Columns[2].Name = "Cadence";
+				dataGV.Columns[3].Name = "Altitude";
+				dataGV.Columns[4].Name = "Power";
+				dataGV.Columns[5].Name = "Power Balance and Pedaling Index";
+
+
+
+				foreach (string line in filelines)
 				{
-					if ((myStream = openFileDialog1.OpenFile()) != null)
+					totalRows++;
+					if (line.Equals("[HRData]"))
 					{
-						using (myStream)
+						rows = y;
+					}
+					y++;
+				}
+
+				int s = totalRows - rows;
+				string[] arrayData = new string[s];
+
+				string heart = "";
+				string speed = "";
+				string cadence = "";
+				string altitude = "";
+				string power = "";
+				string pbpi = "";
+				if (rows != 0)
+				{
+					foreach (string line in filelines)
+					{
+						if (rows < z)
 						{
-							string filename = openFileDialog1.FileName;
-							using (var reader = File.OpenText(filename)) // you need not to use '@filename' instead use just 'filename'
+							data += line + "\r\n";
+							string[] words = line.Split('\t');
+							for (int x = 0; x < words.Length; x++)
 							{
-								string line;
-								int x = 0;
-								int y = 0;
-								string data = "";
-								while ((line = reader.ReadLine()) != null)
-								{
-									string[] parts = line.Split(' ');
-
-									dataGridView1.Columns.Add("colname", "name");
-									dataGridView1.Columns.Add("a", "b");
-									dataGridView1.Columns.Add("c", "d");
-									dataGridView1.Columns.Add("e", "f");
-									dataGridView1.Columns.Add("g", "h");
-									dataGridView1.Columns.Add("i", "j");
-
-									
-
-									
-
-									dataGridView1.Rows.Add();
-									//dataGridView1.DataSource = table;
-								}
+								if (x == 0) heart = words[x];
+								else if (x == 1) speed = words[x];
+								else if (x == 2) cadence = words[x];
+								else if (x == 3) altitude = words[x];
+								else if (x == 4) power = words[x];
+								else pbpi = words[x];
 							}
+							string[] row_val = new string[] { heart, speed, cadence, altitude, power, pbpi };
+							dataGV.Rows.Add(row_val);
+							// System.Console.WriteLine(words[x]);
 						}
+						else z++;
 					}
 				}
+				richTB.Text = data;
 
-				catch  // you need to add the catch block if yo are using try block
+
+				/*
+                StreamReader sr = new StreamReader(filePath);
+                string data = "";
+                while ((oneline = sr.ReadLine()) != null)
+                {
+                    string[] words = oneline.Split(' ');
+                    for(int x = 0; x < words.Length; x++)
+                    {
+                        data += words[x] + "\r\n";
+                    }
+                }
+                txtDisplay.Text = data;
+                */
+			}
+			catch { richTB.Text = "Exception Raised"; }
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog fd = new OpenFileDialog();
+			if (fd.ShowDialog() == DialogResult.OK)
+			{
+				string ext = Path.GetExtension(fd.FileName);
+				if (ext == ".hrm")
 				{
+					filepath = fd.FileName;
+					button1.Visible = true;
+					//lblMessage.Text = "";
 				}
+				else
+				{
+					richTB.Text = "Select only HRM file";
+				}
+
+			}
+		}
+
+		private void selectedColumnButton_Click(object sender, EventArgs e)
+		{
+			Int32 selectedColumnCount = dataGV.Columns.GetColumnCount(DataGridViewElementStates.Selected);
+
+			if (selectedColumnCount > 0)
+			{
+				System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+				for (int i = 0; i < selectedColumnCount; i++)
+				{
+					sb.Append("Column: ");
+					sb.Append(dataGV.SelectedColumns[i].Index.ToString());
+					sb.Append(Environment.NewLine);
+				}
+
+				sb.Append("Total: " + selectedColumnCount.ToString());
+				MessageBox.Show(sb.ToString(), "Selected Columns");
+
 			}
 		}
 	}
 }
+
